882c9f03f0936f842a975678110a3144f7ab45b4
skinhead12 <edithr_isc@hotmail.com>
2023-02-24 12:20:02 -0600

    creando formulario con vue 3

---
+++ form-vue/src/components/SignUpForm.vue
@@ -0,0 +1,90 @@
+<template>
+    <form>
+        <label>Email:</label>
+        <input type="email" v-model="email" required>
+
+        <label>Password:</label>
+        <input type="password" v-model="password" required>
+        <label >Role:</label>
+
+        <select v-model="role">
+            <option value="developer">Web Developer</option>
+            <option value="designer">Wed Designer</option>
+        </select>
+
+        <label>Skills (press alt + comma to add):</label>
+    <input type="text" v-model="tempSkills" @keyup.alt="addSkill">
+    <div v-for="skill in skills" :key="skill" class="pill">
+      {{ skill }}
+    </div>
+
+        <div class="terms">
+        <input type="checkbox" v-model="terms" required>
+
+        <label>Accept terms and conditions </label>
+    </div>
+    </form>
+
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            email: '',
+            password:'',
+            role: '',
+            terms: false,
+            skills:[],
+            tempSkills:'',
+        }
+    },
+    methods:{
+        addSkill($event) {
+      console.log($event)
+      if($event.key === ',' && this.tempSkills) {
+        if (!this.skills.includes(this.tempSkills)) {
+          this.skills.push(this.tempSkills)
+        }
+        this.tempSkills = ''
+      }
+    },
+    }
+}
+</script>
+
+<style>
+form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+  }
+  label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+  input, select {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+  }
+  input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+  }
+</style>
